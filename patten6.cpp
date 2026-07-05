eb52cf68ed963a42827db9849faa1d1966d573b3
Divy <divy92288@gmail.com>
2026-07-05 16:54:19 +0530

    Add latest DSA file updates

---
+++ patten6.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+void print(int n)
+{
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n-i;j++)
+        {
+            cout<<j+1<<" ";
+        }
+        cout<<"\n";
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    print(n);
+}
