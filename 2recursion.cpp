dcf0cf4c933ff5c9774a4d02a803adcfc709b694
Divy <divy92288@gmail.com>
2026-08-11 16:44:05 +0530

    wefuqabckqb

---
+++ 2recursion.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int f(int n)
+{
+    if(n==0)
+    {
+        return 0;
+    }
+    else
+        return n+f(n-1);
+}
+
+int main()
+{
+    int n;
+    cout<<"enter the n";
+    cin>>n;
+    cout<<f(n);
+}
